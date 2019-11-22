977f3cf0a59fd2c000ff561d7edd4ea24630ba9e
hemanyas <56661307+hemanyas@users.noreply.github.com>
2019-11-21 16:51:32 -0800

    Update motors.cpp

--- src/motors.cpp
+++ src/motors.cpp
@@ -5,6 +5,5 @@ pros::Motor left_mtr (LEFT_MTR, pros::E_MOTOR_GEARSET_18, NOT_REVERSE, pros::E_M
 pros::Motor right_mtr (RIGHT_MTR, pros::E_MOTOR_GEARSET_18, REVERSE, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor backright_mtr (BACKRIGHT_MTR, pros::E_MOTOR_GEARSET_18, REVERSE, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor backleft_mtr (BACKLEFT_MTR, pros::E_MOTOR_GEARSET_18, NOT_REVERSE, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor arm_right_mtr (ARM_RIGHT_MTR, pros::E_MOTOR_GEARSET_18, REVERSE, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor arm_left_mtr (ARM_LEFT_MTR, pros::E_MOTOR_GEARSET_18, NOT_REVERSE, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor claw_mtr (CLAW_MTR, pros::E_MOTOR_GEARSET_18, NOT_REVERSE, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor dr4blmtr (DR4BL_MTR, pros::E_MOTOR_GEARSET_18, NOT_REVERSE, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor dr4br_mtr (DR4BR_MTR, pros::E_MOTOR_GEARSET_18, NOT_REVERSE, pros::E_MOTOR_ENCODER_COUNTS);
