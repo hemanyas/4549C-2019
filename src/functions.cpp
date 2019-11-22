461a83336a62f27058b81f449b653f363a1041eb
hemanyas <56661307+hemanyas@users.noreply.github.com>
2019-11-21 16:38:58 -0800

    Update functions.cpp
    dr4b PID

--- src/functions.cpp
+++ src/functions.cpp
@@ -4,11 +4,9 @@
 
 void myPIDforward(float setpoint)
 {
-	float error;
-	float power;
-	float kP = 0.45;
-
-
+float error;
+float power;
+float kP = 0.45;
 {
 float average = (left_mtr.get_position() + right_mtr.get_position() + backright_mtr.get_position() + backleft_mtr.get_position()) / 4;
 error = setpoint - average;
@@ -37,158 +35,60 @@ backright_mtr.move(0);
 }
 
 
-
-
-
-
-
-
-
-
-//
-// void myPIDleft(float setpoint)
-// {
-// 	float error;
-// 	float power;
-// 	float kP = 0.45;
-// 	int ticks_to_deg = 3;
-//
-// do
-// {
-// float average1 = (left_mtr.get_position() + right_mtr.get_position()+ backright_mtr.get_position() + backleft_mtr.get_position()) / 4;
-// error = (setpoint*ticks_to_deg) - average;
-// power = error*kP;
-//
-// printf("power %f\n" , power);
-// printf("error %f\n" , error);
-// printf("average %f\n" , average);
-// printf("setoiubt %f\n" , (setpoint*ticks_to_deg));
-// printf("left_mtr %f\n" , left_mtr.get_position());
-// left_mtr = -power;
-// right_mtr = power;
-// backleft_mtr = -power;
-// backright_mtr = power;
-//
-// pros::delay(20);
-// } while(abs(error) > 5);
-//
-// left_mtr.tare_position();
-// right_mtr.tare_position();
-// backleft_mtr.tare_position();
-// backright_mtr.tare_position();
-//
-// left_mtr.move(0);
-// right_mtr.move(0);
-// backleft_mtr.move(0);
-// backright_mtr.move(0);
-// }
-
-
-
-
-
-
-
-void myPIDarm(float setpoint)
+void myPIDleft(float setpoint)
 {
-	float error;
-	float power;
-	float kP = 1;
-
-
+float error;
+float power;
+float kP = 0.45;
+int ticks_to_deg = 3;
 do
 {
-float average = (arm_left_mtr.get_position() + arm_right_mtr.get_position()) / 2;
-error = setpoint - average;
-power = error*kP;
-//
-printf("power %f\n" , power);
-printf("error %f\n" , error);
-printf("average %f\n" , average);
-arm_left_mtr = power;
-arm_right_mtr = power;
-
-pros::delay(20);
-} while(abs(error) > 10);
-
-arm_left_mtr.tare_position();
-arm_right_mtr.tare_position();
-
-
-arm_right_mtr.move(0);
-}
-
-
-
-
-
-
-
-void myPIDclaw(float setpoint)
-{
-	float error;
-	float power;
-	float kP = 1;
-
-do
-{
-float average = (claw_mtr.get_position());
-error = setpoint - average;
+float average = (left_mtr.get_position() + right_mtr.get_position()+ backright_mtr.get_position() + backleft_mtr.get_position()) / 4;
+error = (setpoint*ticks_to_deg) - average;
 power = error*kP;
 
 printf("power %f\n" , power);
 printf("error %f\n" , error);
 printf("average %f\n" , average);
-claw_mtr = power;
-
+printf("setoiubt %f\n" , (setpoint*ticks_to_deg));
+printf("left_mtr %f\n" , left_mtr.get_position());
+left_mtr = -power;
+right_mtr = power;
+backleft_mtr = -power;
+backright_mtr = power;
 
 pros::delay(20);
-} while(abs(error) > 20);
+} while(abs(error) > 5);
 
-claw_mtr.tare_position();
+left_mtr.tare_position();
+right_mtr.tare_position();
+backleft_mtr.tare_position();
+backright_mtr.tare_position();
 
-claw_mtr.move(0);
-
+left_mtr.move(0);
+right_mtr.move(0);
+backleft_mtr.move(0);
+backright_mtr.move(0);
 }
 
 
-// myCLAW(int s, int d);
-// claw_mtr.move_absolute();
-// pros::delay(20);
-//
-// claw_mtr.tare_position();
-
-
-void myRIGHT(int speed, int time)
+void myPIDdr4b(float setpoint)
 {
-		left_mtr.move(speed);
-  	right_mtr.move(-speed);
-  	backleft_mtr.move(speed);
-  	backright_mtr.move(-speed);
-		pros::delay(time);
-
-
-	left_mtr = 0;
-	right_mtr = 0;
-	backleft_mtr = 0;
-	backright_mtr = 0;
-
-}
-
-
-
-void myLEFT(int speed, int time)
+	float error;
+	float power;
+	float kP = 1;	
+do
 {
-		left_mtr.move(-speed);
-  	right_mtr.move(speed);
-  	backleft_mtr.move(-speed);
-  	backright_mtr.move(speed);
-		pros::delay(time);
-
-
-	left_mtr = 0;
-	right_mtr = 0;
-	backleft_mtr = 0;
-	backright_mtr = 0;
-
+float average = (dr4bl_mtr.get_position() + dr4br_mtr.getposition()) /2;
+float error = setpoint - average;
+float power = error*kp;	
+dr4bl_mtr = power;
+dr4br_mtr = power;
+	
+pros::delay(20);
+}while(fabs(error) > 20);
+dr4bl_mtr.tare_position();
+dr4br_mtr.tare_position();
+dr4bl_mtr.move(0);
+dr4br_mtr.move(0);
 }
