e2aad59e4b25401b6cf75880cbdd5ef2890ef307
hemanyas <56661307+hemanyas@users.noreply.github.com>
2019-11-22 22:26:28 -0800

    Update motors.cpp

--- src/motors.cpp
+++ src/motors.cpp
@@ -7,3 +7,4 @@ pros::Motor backright_mtr (BACKRIGHT_MTR, pros::E_MOTOR_GEARSET_18, REVERSE, pro
 pros::Motor backleft_mtr (BACKLEFT_MTR, pros::E_MOTOR_GEARSET_18, NOT_REVERSE, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor dr4blmtr (DR4BL_MTR, pros::E_MOTOR_GEARSET_18, NOT_REVERSE, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor dr4br_mtr (DR4BR_MTR, pros::E_MOTOR_GEARSET_18, NOT_REVERSE, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor claw_mtr (CLAW_MTR, pros::E_MOTOR_GEARSET_18, NOT_REVERSE, pros::E_MOTOR_ENCODER_COUNTS);
