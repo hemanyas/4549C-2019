b1465d1158d5ce757dfa6171726a31751549366e
hemanyas <56661307+hemanyas@users.noreply.github.com>
2019-11-22 22:25:08 -0800

    Update motors.h

--- include/motors.h
+++ include/motors.h
@@ -8,6 +8,7 @@
 #define BACKRIGHT_MTR 1
 #define DR4BL_MTR 9
 #define DR4BR_MTR 19
+#define CLAW_MTR 12
 
 #define REVERSE 1
 #define NOT_REVERSE 0
@@ -19,6 +20,7 @@ extern pros::Motor backright_mtr;
 extern pros::Motor backleft_mtr;
 extern pros::Motor dr4bl_mtr;
 extern pros::Motor dr4br_mtr;
+extern pros::Motor claw_mtr;
 
 
 
