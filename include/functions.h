43dde749c6c6dadac64168d3f683917b17ca41d7
hemanyas <56661307+hemanyas@users.noreply.github.com>
2019-11-22 22:38:56 -0800

    Update functions.h

--- include/functions.h
+++ include/functions.h
@@ -4,18 +4,11 @@
 #include "main.h"
 #include"motors.h"
 
-
-
-
 void myPIDforward (float setpoint);
-void myPIDreverse(float setpoint);
 void myPIDleft(float setpoint);
-void myPIDright(float setpoint);
-void myPIDarm(float setpoipnt);
-// void myARM(float setpoint);
+void myPIDdr4b(float setpoint);
+void myDR4Bdrive(float setpoint);
 void myPIDclaw(float setpoint);
-void myLEFT(int speed, int time);
-void myRIGHT(int speed, int time);
 
 
 #endif
