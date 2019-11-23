b7ba5067a48cc6c29adb3140e9f928c0aa20a0cf
hemanyas <56661307+hemanyas@users.noreply.github.com>
2019-11-22 22:36:53 -0800

    Update functions.cpp

--- src/functions.cpp
+++ src/functions.cpp
@@ -14,20 +14,16 @@ power = error*kP;
 printf("power_forward %f\n" , power);
 printf("error_forward %f\n" , error);
 printf("average_forward %f\n" , average);
-// printf("left_mtr %f\n" , left_mtr.get_position());
 left_mtr = power;
 right_mtr = power;
 backleft_mtr = power;
 backright_mtr = power;
-
 pros::delay(20);
-} while(abs(error) > 10);
-
+} while(fabs(error) > 10);
 left_mtr.tare_position();
 right_mtr.tare_position();
 backleft_mtr.tare_position();
 backright_mtr.tare_position();
-
 left_mtr.move(0);
 right_mtr.move(0);
 backleft_mtr.move(0);
@@ -46,7 +42,6 @@ do
 float average = (left_mtr.get_position() + right_mtr.get_position()+ backright_mtr.get_position() + backleft_mtr.get_position()) / 4;
 error = (setpoint*ticks_to_deg) - average;
 power = error*kP;
-
 printf("power %f\n" , power);
 printf("error %f\n" , error);
 printf("average %f\n" , average);
@@ -56,15 +51,12 @@ left_mtr = -power;
 right_mtr = power;
 backleft_mtr = -power;
 backright_mtr = power;
-
 pros::delay(20);
-} while(abs(error) > 5);
-
+} while(fabs(error) > 5);
 left_mtr.tare_position();
 right_mtr.tare_position();
 backleft_mtr.tare_position();
 backright_mtr.tare_position();
-
 left_mtr.move(0);
 right_mtr.move(0);
 backleft_mtr.move(0);
@@ -74,21 +66,58 @@ backright_mtr.move(0);
 
 void myPIDdr4b(float setpoint)
 {
-	float error;
-	float power;
-	float kP = 1;	
+float error;
+float power;
+float kP = 1;	
 do
 {
 float average = (dr4bl_mtr.get_position() + dr4br_mtr.getposition()) /2;
 float error = setpoint - average;
 float power = error*kp;	
 dr4bl_mtr = power;
-dr4br_mtr = power;
-	
+dr4br_mtr = power;	
 pros::delay(20);
 }while(fabs(error) > 20);
 dr4bl_mtr.tare_position();
 dr4br_mtr.tare_position();
 dr4bl_mtr.move(0);
 dr4br_mtr.move(0);
+}
+
+void myDR4Bdrive(float setpoint);
+{
+float error;
+float power;
+float kP = 1;
+do 
+{
+float average = (dr4bl_mtr.get_position() + dr4br_mtr.getposition()) /2;
+float error = setpoint - average;
+float power = error*kp;	
+dr4bl_mtr = power;
+dr4br_mtr = power;	
+pros::delay(20);
+}while(fabs(error) > 20);
+dr4bl_mtr.tare_position();
+dr4br_mtr.tare_position();
+dr4bl_mtr.move(0);
+dr4br_mtr.move(0);
+}
+
+
+void myPIDclaw(float setpoint)
+{
+float error;
+float power;
+float kP = 1;
+do
+{
+float average = (claw_mtr.get_position());
+float eroor = setpoint - average;
+float power = error*kP;
+claw_mtr= power;
+pros::delay(20);
+}while(fabs(error)> 20);
+claw_mtr.tare_position();
+claw_mtr.move(0);
 }
